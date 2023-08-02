ca677e377b431190ec0740438b0d0ecccb07aad3
Sadman Sakib <mahmudshadman@gmail.com>
2023-08-02 22:18:11 +0600

    Added loading animation and add movie form.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,78 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+import LoadingEffect from "./components/LoadingEffect";
+import AddMovie from "./components/AddMovie";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  //saving the function reference so that is does not re-created every time the component re-render.
+  const getMoviesHandler = useCallback(async () => {
+    try {
+      setError(null);
+      setIsLoading(true);
+      const apiKey = "api_key=ef64498e81f793cbb78eaef9c83167da";
+      const baseUrl = "https://api.themoviedb.org/3";
+      const apiUrl =
+        baseUrl + "/discover/movie?sort_by=popularity.desc&" + apiKey;
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const data = await response.json();
+      console.log(data);
+      const fetchedMovies = data.results.map((movie) => {
+        return {
+          id: movie.id,
+          title: movie.original_title,
+          openingText: movie.overview,
+          releaseDate: movie.release_date,
+        };
+      });
+      setMovies(fetchedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    console.log("using use effect");
+    getMoviesHandler();
+  }, [getMoviesHandler]);
+
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
+  let content;
+  if (!error && !isLoading && movies.length <= 0) {
+    content = <p>No movies found</p>;
+  }
+  if (error && !isLoading) {
+    content = <p>Somethign went wrong. Try again later.</p>;
+  }
+  if (isLoading) {
+    content = <LoadingEffect />;
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <button onClick={getMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {content}
+        {!error && !isLoading && movies.length > 0 && (
+          <MoviesList movies={movies} />
+        )}
       </section>
     </React.Fragment>
   );
---
+++ src/components/LoadingEffect.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./LoadingEffect.css";
+
+const LoadingEffect = (props) => {
+  return (
+    <React.Fragment>
+      <div className="loading"></div>
+      <p>Loading</p>
+    </React.Fragment>
+  );
+};
+
+export default LoadingEffect;
